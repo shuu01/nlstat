3d505203e3cda5fa87c702a66a18b48920981a4f
shuu01 <shuu01@gmail.com>
2019-05-08 12:14:23 +0300

    convert lists to generators, just for fun

--- dclnt.py
+++ dclnt.py
@@ -3,7 +3,7 @@
 import ast
 import os
 import collections
-from typing import List, Tuple, Any
+from typing import List, Tuple, Any, Generator
 
 import nltk
 from nltk import pos_tag
@@ -13,6 +13,7 @@ if not nltk.data.find('taggers/averaged_perceptron_tagger'):
 
 # count of files that will be parsed in path
 FILES_LIMIT: int = 100
+
 
 def is_verb(word: str) -> bool:
 
@@ -29,31 +30,33 @@ def is_magic(name: str) -> bool:
     return False
 
 
-def get_filenames(path: str) -> List[str]:
+def get_filenames(path: str) -> Generator[str, None, None]:
     '''
         path: filepath
         search for files from path with ".py" extension
         return: list of filenames
     '''
 
-    filenames: List[str] = []
+    count: int = 0
 
     for dirname, dirs, files in os.walk(path, topdown=True):
         for file in files:
             if file.endswith('.py'):
-                filenames.append(os.path.join(dirname, file))
-            if len(filenames) == FILES_LIMIT:
-                print('total {} files'.format(len(filenames)))
-                return filenames
+                count += 1
+                yield os.path.join(dirname, file)
+            if count == FILES_LIMIT:
+                print('total {} files'.format(count))
+                return None
 
-    print('total {} files'.format(len(filenames)))
-    return filenames
+    print('total {} files'.format(count))
+    return
 
 
 def get_trees(
     path: str,
-    with_filenames: bool=False,
-    with_file_content: bool=False) -> List[Any]:
+    with_filenames: bool = False,
+    with_file_content: bool = False,
+) -> Generator[Any, None, None]:
     '''
         path: filepath
         get all files from path with ".py" extension and build
@@ -61,61 +64,54 @@ def get_trees(
         return: list of trees
     '''
 
-    filenames: List[str] = get_filenames(path)
-    trees: List[Any] = []
-
-    for filename in filenames:
+    for filename in get_filenames(path):
 
         with open(filename, 'r', encoding='utf-8') as attempt_handler:
             main_file_content = attempt_handler.read()
 
         try:
             tree = ast.parse(main_file_content)
-            print(tree, type(tree))
         except SyntaxError as e:
             print(filename, e)
             tree = ast.Module(None)
 
         if with_filenames:
             if with_file_content:
-                trees.append((filename, main_file_content, tree))
+                yield (filename, main_file_content)
             else:
-                trees.append((filename, tree))
+                yield (filename, tree)
         else:
-            trees.append(tree)
-
-    print('trees generated')
-
-    return trees
+            yield tree
 
 
-def get_all_names(tree: ast.Module) -> List[str]:
-    return [node.id for node in ast.walk(tree) if isinstance(node, ast.Name)]
+def get_all_names(tree: ast.Module) -> Generator[str, None, None]:
+    return (node.id for node in ast.walk(tree) if isinstance(node, ast.Name))
 
 
-def get_verbs_from_function_name(function_name: str) -> List[str]:
-    return [word for word in function_name.split('_') if is_verb(word)]
+def get_verbs_from_function_name(
+    function_name: str
+) -> Generator[str, None, None]:
+    return (word for word in function_name.split('_') if is_verb(word))
 
 
-def get_all_words_in_path(path: str) -> List[str]:
+def get_all_words_in_path(path: str) -> Generator[str, None, None]:
     '''
         path: filepath
         build abstract syntax tree from path and get all words from tree
         return: list of words
     '''
 
-    trees: List[Any] = get_trees(path)
-
-    words: List[str] = []
-    for tree in trees:
+    for tree in get_trees(path):
         for name in get_all_names(tree):
             if not is_magic(name):
-                words.extend(name.split('_'))
+                for x in name.split('_'):
+                    yield x
 
-    return words
 
-
-def get_top_verbs_in_path(path: str, top_size: int=10) -> List[Tuple[Any, int]]:
+def get_top_verbs_in_path(
+    path: str,
+    top_size: int = 10,
+) -> List[Tuple[Any, int]]:
     '''
         path: filepath
         get all function names from python files in path,
@@ -123,18 +119,18 @@ def get_top_verbs_in_path(path: str, top_size: int=10) -> List[Tuple[Any, int]]:
         return: list of tuples
     '''
 
-    names: List[str] = get_all_function_names_in_path(path)
-    print('functions extracted')
-
     verbs: List[str] = []
-    for function_name in names:
+    for function_name in get_all_function_names_in_path(path):
         verbs.extend(get_verbs_from_function_name(function_name))
     print('verbs extracted')
 
     return get_top(verbs, top_size)
 
 
-def get_top_functions_names_in_path(path: str, top_size: int=10) -> List[Tuple[Any, int]]:
+def get_top_functions_names_in_path(
+    path: str,
+    top_size: int = 10,
+) -> List[Tuple[Any, int]]:
     '''
         path: filepath
         get all function names from python files in path
@@ -142,31 +138,26 @@ def get_top_functions_names_in_path(path: str, top_size: int=10) -> List[Tuple[A
         return: list of tuples
     '''
 
-    names = get_all_function_names_in_path(path)
+    names = [get_all_function_names_in_path(path)]
 
     return get_top(names, top_size)
 
 
-def get_all_function_names_in_path(path: str) -> List[str]:
+def get_all_function_names_in_path(path: str) -> Generator[str, None, None]:
     '''
         path: filepath
         return: all non dunder function names from path
     '''
 
-    trees: List[Any] = get_trees(path)
-
-    names: List[str] = []
-    for tree in trees:
+    for tree in get_trees(path):
         for node in ast.walk(tree):
             if isinstance(node, ast.FunctionDef):
                 name = node.name.lower()
                 if not is_magic(name):
-                    names.append(name)
-
-    return names
+                    yield name
 
 
-def get_top(words: List[Any], top_size: int=10) -> List[Tuple[Any, int]]:
+def get_top(words: List[Any], top_size: int = 10) -> List[Tuple[Any, int]]:
     return collections.Counter(words).most_common(top_size)
 
 
